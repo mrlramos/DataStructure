c6e1d90929c3dc546368931324b1fc53a3c87d17
Marlon Ramos <marlon_raoni10@hotmail.com>
2023-09-22 20:25:27 -0300

    SimpleQueue

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # DataStructure
 Repository to practice some algorithm implementation
+
+1 - SimpleQueue
+A program that implements a simple queue in C#
--- SimpleQueue/Program.cs
+++ SimpleQueue/Program.cs
@@ -1,2 +1,48 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+Queue<string> queue = new();
+
+ListAll();
+ListCount();
+
+Insert("Marlon");
+
+ListAll();
+ListCount();
+
+Remove();
+
+ListAll();
+ListCount();
+
+Console.ReadLine();
+
+void Insert(string person)
+{
+    queue.Enqueue(person);
+}
+
+void Remove()
+{
+    queue.Dequeue();
+}
+
+void ListAll()
+{
+    if (queue.Count > 0)
+    {
+        foreach (string person in queue)
+        {
+            Console.WriteLine(person);
+        }
+    } 
+    else
+    {
+        Console.WriteLine("Queue empty");
+    }
+}
+
+void ListCount()
+{
+    Console.WriteLine(queue.Count);
+}
