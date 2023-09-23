3eef4d84b6776746c9b8713866bafa61bc919211
Marlon Ramos <marlon_raoni10@hotmail.com>
2023-09-23 11:49:55 -0300

    2-SimpleManualQueue

---
+++ 2-SimpleManualQueue/Program.cs
@@ -0,0 +1,66 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string[] queue = Array.Empty<string>();
+short count = 0;
+
+ListAll();
+ListCount();
+
+Insert("Marlon");
+
+ListAll();
+ListCount();
+
+Remove();
+
+ListAll();
+ListCount();
+
+Console.ReadLine();
+
+void Insert(string person)
+{
+    Array.Resize(ref queue, ++count);
+    queue[count - 1] = person;
+}
+
+void Remove()
+{
+    if (count == 0)
+    {
+        throw new InvalidOperationException($"Queue empty");
+    }
+
+    string elementRemoved = queue[0];
+
+    if (count > 1)
+    {
+        for (short i = 0; i < count; i++)
+        {
+            queue[i] = queue[i + 1];
+        }
+    }  
+
+    Array.Resize(ref queue, --count);
+    Console.WriteLine($"Element removed {elementRemoved}");
+}
+
+void ListAll()
+{
+    if (queue.Length > 0)
+    {
+        for (short i = 0; i < queue.Length; i++)
+        {
+            Console.WriteLine(queue[i]);
+        }
+    }
+    else
+    {
+        Console.WriteLine("Queue empty");
+    }
+}
+
+void ListCount()
+{
+    Console.WriteLine(queue.Length);
+}
--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # DataStructure
 Repository to practice some algorithm implementation
 
-1 - SimpleQueue
-A program that implements a simple queue in C#
+1 - SimpleQueue: 
+A program that implements a simple queue in C# using Queue class
+
+2 - SimpleManualQueue:
+A program that implements a simple manual queue in C#
