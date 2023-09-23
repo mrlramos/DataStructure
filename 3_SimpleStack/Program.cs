24f8236015231a618953b251e0ce3ef6f3e8ed32
Marlon Ramos <marlon_raoni10@hotmail.com>
2023-09-23 14:40:48 -0300

    Simple Stack

---
+++ 3_SimpleStack/Program.cs
@@ -0,0 +1,53 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Stack<double> stack = new();
+
+IsEmpty();
+Peek();
+
+Push(74.504);
+Push(1.73);
+
+IsEmpty();
+Peek();
+
+Pop();
+
+IsEmpty();
+Peek();
+
+Console.ReadLine();
+
+void Push(double number)
+{
+    stack.Push(number);
+}
+
+void Pop()
+{
+    stack.Pop();
+}
+
+void Peek()
+{
+    if (stack.Count != 0)
+    {
+        Console.WriteLine($"{stack.First()}");
+    }
+    else
+    {
+        Console.WriteLine("Stack empty");
+    }
+}
+
+void IsEmpty()
+{
+    if (stack.Count == 0)
+    {
+        Console.WriteLine($"Stack empty");
+    }
+    else
+    {
+        Console.WriteLine("The stack contains data");
+    }
+}
--- README.md
+++ README.md
@@ -6,3 +6,6 @@ A program that implements a simple queue in C# using Queue class
 
 2 - SimpleManualQueue:
 A program that implements a simple manual queue in C#
+
+2 - SimpleStack:
+A program that implements a simple stack in C# using Stack class
