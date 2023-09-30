e8f31ae40de4e71ff75a3d6db3fcdf55b77eb6b8
Marlon Ramos <marlon_raoni10@hotmail.com>
2023-09-30 11:47:35 -0300

    Improvements in QuickSort and tests created

--- 5_QuickSort/Program.cs
+++ 5_QuickSort/Program.cs
@@ -1,5 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using _5_QuickSort;
+
 int[] ages = { 5, 20, 4, 2};
 
 for (int i = 0; i < ages.Length; i++)
@@ -7,7 +9,9 @@ for (int i = 0; i < ages.Length; i++)
     Console.WriteLine(ages[i]);
 }
 
-QuickSort(ages, 0, ages.Length - 1);
+QuickSort quickSort = new();
+
+quickSort.Sort(ref ages, 0, ages.Length - 1);
 Console.WriteLine("\n");
 
 for (int i = 0; i< ages.Length; i++)
@@ -17,34 +21,3 @@ for (int i = 0; i< ages.Length; i++)
 
 Console.ReadLine();
 
-void QuickSort(int[] v, int start_pos, int end_pos)
-{
-    var pivot = v[start_pos];
-    int l = start_pos;
-    int r = end_pos;
-
-    while (l <= r) 
-    {
-        while (v[l] < pivot) l++;
-        while (v[r] > pivot) r--;
-        
-        if (l <= r) 
-        {
-            int temp = v[l];
-            v[l] = v[r];
-            v[r] = temp;
-            //(v[l], v[r]) = (v[r], v[l]); do the same thing at three lines above
-            l++;
-            r--;
-        }
-    }
-
-    if (start_pos < r )
-    {
-        QuickSort(v, start_pos, r);
-    }
-    if (l < end_pos)
-    {
-        QuickSort(v, l, end_pos);
-    }
-}
